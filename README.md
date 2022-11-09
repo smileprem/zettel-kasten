29a2883211afcf08e85d65f81d16cbe7cfc49d42
Premkumar Masilamani <smileprem@users.noreply.github.com>
2022-11-09 14:26:49 -0800

    Made the ZettelKasten path as configurable parameter

--- README.md
+++ README.md
@@ -1,5 +1,17 @@
 # Zettel Kasten
 Simple implementation of Zettel Kasten
 
+## Configuration
+
+Create a file in `~/.zettel.json` with the following content:
+
+```json
+{
+    "zettelkasten": "/path/to/zettelkasten"
+}
+```
+
 # Build
- go build -o zettel
+```
+go build -o zettel
+```
--- main.go
+++ main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"bufio"
+	"encoding/json"
 	"fmt"
+	"io"
 	"os"
 	"os/exec"
 	"strings"
@@ -13,7 +15,8 @@ import (
 
 const TimeFormat = "20060102150405"
 const NewLineChar = '\n'
-const ZettelKastenLocation = "/Users/smileprem/Documents/Obsidian/smileprem/zettelkasten/"
+const DefaultZettelConfigFile = "/Users/smileprem/.zettel.json"
+const DefaultZettelKastenLocation = "./"
 
 func main() {
 	fmt.Println("------------------------------------------")
@@ -27,13 +30,13 @@ func main() {
 	fmt.Print("title: ")
 	zettelTitle, err := reader.ReadString(NewLineChar)
 	if err != nil {
-		fmt.Println("Error reading the title", err.Error())
+		fmt.Println("Error reading the title. Error: ", err.Error())
 	}
 
 	fmt.Print("tags (comma de-limited): ")
 	zettelTags, err := reader.ReadString(NewLineChar)
 	if err != nil {
-		fmt.Println("Error reading the tags", err.Error())
+		fmt.Println("Error reading the tags. Error: ", err.Error())
 	}
 
 	zettelTemplate += "# " + zettelID + " " + zettelTitle
@@ -44,12 +47,12 @@ func main() {
 
 	zettelFileName, err := createZettelFile(zettelID, zettelTitle, zettelTemplate)
 	if err != nil {
-		fmt.Println("Error creating the zettel file", err.Error())
+		fmt.Println("Error creating the zettel file. Error: ", err.Error())
 	}
 
 	err = exec.Command("code", zettelFileName).Run()
 	if err != nil {
-		fmt.Println("Error opening vscode with the zettel file", err.Error())
+		fmt.Println("Error opening vscode with the zettel file. Error: ", err.Error())
 	}
 }
 
@@ -62,7 +65,7 @@ func formatZettelTags(zettelTags string) string {
 }
 
 func createZettelFile(zettelID string, zettelTitle string, zettelTemplate string) (string, error) {
-	zettelFilename := ZettelKastenLocation + zettelID + "-" + strcase.ToKebab(zettelTitle) + ".md"
+	zettelFilename := getZettelFileNameFromConfig(zettelID, zettelTitle)
 	zettelFile, err := os.Create(zettelFilename)
 	if err != nil {
 		fmt.Println(err.Error())
@@ -73,4 +76,23 @@ func createZettelFile(zettelID string, zettelTitle string, zettelTemplate string
 		return "", err
 	}
 	return zettelFilename, nil
+}
+
+func getZettelFileNameFromConfig(zettelID string, zettelTitle string) string {
+	zettelKastenPath := DefaultZettelKastenLocation
+	jsonFile, err := os.Open(DefaultZettelConfigFile)
+	if err != nil {
+		fmt.Println("Unable to open the zettel config file. Creating zettel in current directory. Error: ", err.Error())
+	} else {
+		byteValue, _ := io.ReadAll(jsonFile)
+		var result map[string]interface{}
+		err = json.Unmarshal(byteValue, &result)
+		if err != nil {
+			fmt.Println("Zettel config file is not a proper json. Creating zettel in current directory. Error: ", err.Error())
+		}
+		zettelKastenPath = result["zettelkasten"].(string)
+	}
+	_ = jsonFile.Close()
+
+	return zettelKastenPath + zettelID + "-" + strcase.ToKebab(zettelTitle) + ".md"
 }
